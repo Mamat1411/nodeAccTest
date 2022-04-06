e40919055e964b4151303d87c3018e7e442d1a81
Mamat1411 <muhammadmujahid14@gmail.com>
2022-04-06 10:38:35 +0700

    Initial Simple API for postman hit simulation

--- app.js
+++ app.js
@@ -1,3 +1,16 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+
+app.post('/', (req, res) => {
+    res.send("Hello World");
+});
+
+app.use('/', (req, res) => {
+    res.status(404);
+    res.send(`404`);
+});
+
+app.listen(port, () =>{
+    console.log(`This App is listening to port ${port}`);
+});
